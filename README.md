be04ef18b0c61bb259fcf0d1f34413d963006dff
apyvision <team@apy.vision>
2021-05-17 16:48:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# APY Vision LP Balances Subgraph
+
+#### Introduction 
+This subgraph will index all the Liquidity Pool balances of a user whether they are still in the pool or not. This is useful information as we want to build a history of P+L for their liquidity pool holdings. 
+
+Supports Balancer, Sushiswap and Uniswap -- more to be added later.
+
+This is one of the subgraphs used for [APY Vision](https://apy.vision)
+
+#### Building
+
+First, change your node version
+```
+nvm use 10.16.3
+```
+
+To generate the mapping ts files, please do:
+```
+yarn codegen
+```
+To deploy, please use:
+```
+ graph deploy \
+    --debug \
+    --node https://api.thegraph.com/deploy/ \
+    --ipfs https://api.thegraph.com/ipfs/ \
+    apyvision/fantom-amm-lp-shares
+```
+
+
+#### Schema
+
+```
+User => id, LiquidityPositions
+LiquidityPosition => id, poolAddress, balance, poolProviderName (one of Balancer, Uniswap, Sushiswap and YFV)
+```
+
+
+#### Contributions
+If you have other AMMs to add (ie. Mooniswap, Moonswap, etc), please feel free to open a PR!
---
+++ src/spooky/mapping.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prefer-const */
+import {Address, log} from '@graphprotocol/graph-ts'
+import {Transfer} from '../../generated/templates/SpookyPair/Pair'
+import {
+  ADDRESS_ZERO,
+  createOrUpdateLiquidityPosition,
+  createTransferEvent,
+  MINUS_ONE,
+  updateDayData,
+  ZERO_BI
+} from "../util";
+
+let PROVIDER_KEY = "spookyswap_fantom";
+
+export function handleTransfer(event: Transfer): void {
+  let poolAddress = event.address;
+  let to = event.params.to as Address;
+  let from = event.params.from as Address;
+  let amt = event.params.value;
+
+  if (to.toHexString() == ADDRESS_ZERO) { // BURN
+    log.warning("[{}] BURN event for tx {} for user {} with amount {}", [PROVIDER_KEY, event.transaction.hash.toHexString(), from.toHexString(), amt.toString()])
+
+    updateDayData(createOrUpdateLiquidityPosition(PROVIDER_KEY, poolAddress, from, amt.times(MINUS_ONE)), from, event);
+    createTransferEvent(event, from, from, to, amt)
+
+  } else if (from.toHexString() == ADDRESS_ZERO) { // MINT
+    log.warning("[{}] MINT event for tx {} for user {} with amount {}", [PROVIDER_KEY, event.transaction.hash.toHexString(), to.toHexString(), amt.toString()])
+
+    updateDayData(createOrUpdateLiquidityPosition(PROVIDER_KEY, poolAddress, to, amt), to, event);
+    createTransferEvent(event, to, from, to, amt)
+
+  } else { // TRANSFER
+    updateDayData(createOrUpdateLiquidityPosition(PROVIDER_KEY, poolAddress, to, ZERO_BI), to, event);
+    updateDayData(createOrUpdateLiquidityPosition(PROVIDER_KEY, poolAddress, from, ZERO_BI), from, event);
+    // we don't need to keep track of the initiator here because the transfer logs will have logged the lp token transfer event
+    createTransferEvent(event, from, from, to, amt)
+    createTransferEvent(event, to, from, to, amt)
+
+  }
+}
+
